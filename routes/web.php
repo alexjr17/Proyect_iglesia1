ab7248c881071ad404f36a86035681b91ffd978b
alexjr17 <alexjose.r.r@gmail.com>
2022-04-06 20:23:08 -0500

    c

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/admin/miembros', [MiembroController::class, 'index'])->name('admin.miembros.index');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
