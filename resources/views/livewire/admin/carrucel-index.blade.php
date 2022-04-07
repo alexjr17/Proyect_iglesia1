da48de831b3ee096c7619b348fed207729292047
alexjr17 <alexjose.r.r@gmail.com>
2022-04-07 00:07:24 -0500

    c

--- resources/views/livewire/admin/carrucel-index.blade.php
+++ resources/views/livewire/admin/carrucel-index.blade.php
@@ -1,7 +1,12 @@
 <div>
     @foreach ($carrucels as $carrucel)
         <div class="carrucel static">
-            <img class="rounded-lg static w-full" src="{{ Storage::url($carrucel->image->url) }}" alt="">
+            @isset($carrucel->image->url)
+                <img class="rounded-lg static w-full" src="{{ Storage::url($carrucel->image->url) }}" alt="">
+            @else
+                <img id="picture" class="h-40 w-40 rounded-full" id=""
+                    src="https://www.freejpg.com.ar/asset/900/d0/d08d/F100008153.jpg" alt="">
+            @endisset
             <div class="grid grid-cols-1 content-between absolute top-0 right-0 bg-indigo-500/60 h-full">
                 <div class="flex-row w-40 bg-indigo-700/60 rounded-md">
                     <h1 class="bg-green-600 w-full pb-1 z-10 text-center">
